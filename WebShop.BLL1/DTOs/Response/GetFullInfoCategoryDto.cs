3e8798cc88b00c0246214f7ea8fe93fa00ea9d6d
kollt <55020297+kollt@users.noreply.github.com>
2024-08-21 17:46:26 +0300

    Create context

---
+++ WebShop.BLL1/DTOs/Response/GetFullInfoCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿using WebShop.DAL.Entities;
+namespace WebShop.BLL.DTOs.Response;
+
+public class GetFullInfoCategoryDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public List<Products>? Products { get; set; }
+}
